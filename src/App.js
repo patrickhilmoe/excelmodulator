6af6192de11dca8de22b8df269b18cf29579fff3
Patrick Hilmoe <pshilmoe@gmail.com>
2022-05-05 21:04:38 -0700

    fouth commit

--- src/App.js
+++ src/App.js
@@ -2,10 +2,15 @@ import "./App.css";
 
 function App() {
   return (
+    <>
     <div>
       Hello, React App is working
       and updatd text
     </div>
+    <div>
+      with even more text
+    </div>
+    </>
     );
 }
 
